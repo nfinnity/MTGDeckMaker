e30df8d6fac9336bcb160ea7d5cc47b4753189f0
KeyMasterXV <vinicius.cabralxv@gmail.com>
2019-02-10 22:38:11 -0500

    Initial commit for setting up the project. Includes: 1. New module for the application and initial Maven dependencies 2. Controller with URL mapping for hitting new API for card searches

---
+++ mtg-deck-maker/src/test/java/com/cabralduo/mtgdeckmaker/MtgDeckMakerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cabralduo.mtgdeckmaker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MtgDeckMakerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
